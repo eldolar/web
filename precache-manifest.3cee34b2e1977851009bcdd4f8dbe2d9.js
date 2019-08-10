6e061a23eeba7dc3ef0643a3a75c3af807af32c4
Edu Depetris <edu.depetris.00@gmail.com>
2019-08-10 16:01:08 -0300

    Updates

---
+++ precache-manifest.3cee34b2e1977851009bcdd4f8dbe2d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9279fc016d12d12161973332578b01a",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "8f292b3292f2b7af019d",
+    "url": "/web/static/css/main.82c78356.chunk.css"
+  },
+  {
+    "revision": "24a5d4f3250b7863b5c1",
+    "url": "/web/static/js/2.ac4fe67a.chunk.js"
+  },
+  {
+    "revision": "8f292b3292f2b7af019d",
+    "url": "/web/static/js/main.144084be.chunk.js"
+  },
+  {
+    "revision": "33ed4ec846e9d84e8327",
+    "url": "/web/static/js/runtime~main.c75ffd02.js"
+  }
+]);
